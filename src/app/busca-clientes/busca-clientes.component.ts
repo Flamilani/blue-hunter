c2d10b41c292e492ad8e92ec7ccc0a0f9aa831b4
Flavio Milani <milanidesign7@gmail.com>
2018-04-19 09:36:35 -0300

    criado app busca clientes

---
+++ src/app/busca-clientes/busca-clientes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-busca-clientes',
+  templateUrl: './busca-clientes.component.html',
+  styleUrls: ['./busca-clientes.component.css']
+})
+export class BuscaClientesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
